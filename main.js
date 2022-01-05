242eb68eae8a2fb5bd3c6c58478828850817866d
ayush595 <81427649+ayush595@users.noreply.github.com>
2022-01-05 14:03:49 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+leftwrist=0;
+rightwrist=0;
+difference=0;
+nose= ;
+function setup() {
+    video = createCapture(VIDEO);
+    video.size()
+    canvas = createCanvas(500,500) ;
+modelattacher = ml5.poseNet(video , modelLoaded);
+}
+function modelLoaded() {
+    console.log("model is loaded mam/sir");
+}
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results);
+        leftwrist = results[0].pose.leftWrist.x;
+        rightwrist = results[0].pose.rightWrist.x;
+difference = Math.floor(leftwrist-rightwrist);
+
+    }
+}
+function draw() {
+ backgrounnd("white");
+ Fill("red");
+ textSize(difference);
+ text("ayush is best",50,400);
+
+}
